d408e62c4d40192e1e4d3353a7d7c5883e483fc3
andy <andy@andydote.co.uk>
2017-09-08 16:21:43 +0300

    solution: implement project adding

--- src/solution.js
+++ src/solution.js
@@ -1,15 +1,18 @@
-const defaultLines = [
+const headerLines = [
   'Microsoft Visual Studio Solution File, Format Version 12.00',
   '# Visual Studio 14',
   'VisualStudioVersion = 14.0.25420.1',
-  'MinimumVisualStudioVersion = 10.0.40219.1',
+  'MinimumVisualStudioVersion = 10.0.40219.1'
+]
+
+const defaultLines = [
   'Global',
   '	GlobalSection(SolutionConfigurationPlatforms) = preSolution',
   '		Debug|Any CPU = Debug|Any CPU',
   '		Release|Any CPU = Release|Any CPU',
-  '	EndGlobalSection',
-  '	GlobalSection(ProjectConfigurationPlatforms) = postSolution',
-  '	EndGlobalSection',
+  '	EndGlobalSection'
+]
+const afterLines = [
   '	GlobalSection(SolutionProperties) = preSolution',
   '		HideSolutionNode = FALSE',
   '	EndGlobalSection',
@@ -19,13 +22,48 @@ const defaultLines = [
   ''
 ]
 
+const types = {
+  project: 'FAE04EC0-301F-11D3-BF4B-00C04F79EFBC'
+}
+
+const configurations = ['Debug|Any CPU', 'Release|Any CPU']
+
+const projects = []
+
 const solution = {
-  addProject: function(project) {},
+  addProject: function(project) {
+    projects.push(project)
+  },
 
   removeProject: function(project) {},
 
   writeTo: function(writer) {
+    headerLines.forEach(line => writer.writeLine(line))
+    this.writeProjects(writer)
     defaultLines.forEach(line => writer.writeLine(line))
+    this.writePlatforms(writer)
+    afterLines.forEach(line => writer.writeLine(line))
+  },
+
+  writeProjects: function(w) {
+    projects.forEach(project => {
+      w.writeLine(
+        `Project("{${types.project}}") = "${project.name}", "${project.path}", "{${project.id}}"`
+      )
+      w.writeLine(`EndProject`)
+    })
+  },
+
+  writePlatforms: function(w) {
+    w.writeLine('\tGlobalSection(ProjectConfigurationPlatforms) = postSolution')
+
+    projects.forEach(project => {
+      configurations.forEach(config => {
+        w.writeLine(`\t\t{${project.id}}.${config}.ActiveCfg = ${config}`)
+        w.writeLine(`\t\t{${project.id}}.${config}.Build.0 = ${config}`)
+      })
+    })
+    w.writeLine('	EndGlobalSection')
   }
 }
 
--- src/solution.test.js
+++ src/solution.test.js
@@ -1,10 +1,11 @@
 import solution from './solution'
 import fs from 'fs'
 
-const blankSolution = fs
-  .readFileSync('./test-data/blank.sln')
-  .toString()
-  .split(/\r?\n/)
+const linesOf = name =>
+  fs
+    .readFileSync(`./test-data/${name}.sln`)
+    .toString()
+    .split(/\r?\n/)
 
 it('should be able to output a blank solution file', () => {
   const lines = []
@@ -14,5 +15,21 @@ it('should be able to output a blank solution file', () => {
 
   solution.writeTo(writer)
 
-  expect(lines).toEqual(blankSolution)
+  expect(lines).toEqual(linesOf('blank'))
+})
+
+it('should write a single project to the solution file', () => {
+  const lines = []
+  const writer = {
+    writeLine: line => lines.push(line)
+  }
+
+  solution.addProject({
+    id: '3F5F5B05-7B61-43BF-9217-7E2005FAEF68',
+    name: 'ProjectUnderRoot',
+    path: 'ProjectUnderRoot\\ProjectUnderRoot.csproj'
+  })
+  solution.writeTo(writer)
+
+  expect(lines).toEqual(linesOf('oneProject'))
 })
