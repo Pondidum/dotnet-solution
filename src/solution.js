8883789e10a99c79efbc768502b3dbfb296b259d
andy <andy@andydote.co.uk>
2017-09-14 16:37:16 +0100

    solution: support parentName lookup on add

--- src/solution.js
+++ src/solution.js
@@ -30,11 +30,25 @@ class Solution {
   }
 
   addFolder(folder) {
-    this.children.push(Object.assign({}, folder, { type: ChildTypes.folder }))
+    this.add(folder, ChildTypes.folder)
   }
 
   addProject(project) {
-    this.children.push(Object.assign({}, project, { type: ChildTypes.project }))
+    this.add(project, ChildTypes.project)
+  }
+
+  add(item, type) {
+    let parentName = item.parent
+    if (item.parent) {
+      const parent = this.children.find(child => child.id === item.parent)
+      if (parent) {
+        parentName = parent.name
+      }
+    }
+
+    this.children.push(
+      Object.assign({}, item, { type: type, parent: parentName })
+    )
   }
 
   writeTo(writer) {
