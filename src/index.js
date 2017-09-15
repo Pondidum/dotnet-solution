5888a70b92858c240aa42bda05ecbf1d1aad76f8
andy <andy@andydote.co.uk>
2017-09-15 16:20:56 +0100

    package: release prep

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Solution from './solution'
+import SolutionReader from './solutionReader'
+import SolutionWriter from './solutionWriter'
+
+export Solution
+export SolutionReader
+export SolutionWriter
